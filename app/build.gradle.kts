f7571636a5e720c628c101a72ef6b2b7af980cb8
vincentbonsi <bons0017@algonquinlive.com>
2023-11-15 11:03:34 -0500

    got it running (targetSdk still underlined in red)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "algonquin.cst2335.bonsilabs6plus"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "algonquin.cst2335.bonsilabs6plus"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
